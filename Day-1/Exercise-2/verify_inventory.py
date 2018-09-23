8a49fee267757314f88c73f1669481ddd026ee3e
codeburster <codeburster@gmail.com>
2018-09-24 04:12:52 +0530

    Ex-3 Ansible config practice and Ex-4 Ad-Hoc command practice Added

--- Day-1/Exercise-2/verify_inventory.py
+++ Day-1/Exercise-2/verify_inventory.py
@@ -12,10 +12,10 @@ window = Tk()
 window.title("Verifying Ansible Inventory")
 window.geometry("+450+180")
 if err :
-    lbl = Label(window,text="Ooops !!\nThat didn't work !!",font=("Arial Black", 20),bg="yellow",fg="Dark Blue")
+    lbl = Label(window,text="Ooops !!\nThat didn't work !!\nError: \n"+err,font=("Arial Black", 18),bg="yellow",fg="Dark Blue")
     lbl.pack(side="top",padx=10,pady=100)
 else :
-    lbl = Label(window,text="Congratulations !!\nYou did it, You are a freaking Genius !!",font=("Arial Black", 20),bg="yellow",fg="Dark Blue")
+    lbl = Label(window,text="Congratulations !!\nYou did it, You are a freaking Genius !!"+output,font=("Arial Black", 20),bg="yellow",fg="Dark Blue")
     lbl.pack(side="top",padx=10,pady=100)
 
 window.mainloop()
